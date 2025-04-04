b842d02481aea035c0a1b7b88677c0c5fb345618
serious03black <prathaamesh2509@gmail.com>
2025-04-04 14:06:54 +0530

    pakka final

--- Testemonials/fetchReview.js
+++ Testemonials/fetchReview.js
@@ -1,53 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-   
-
-    const container = document.getElementById("cards");
-   
-
-    let allReviws = []; // Store fetched data
+  const container = document.getElementById("cards");
+  let allReviews = []; // Store fetched data
 
   // Fetch data from JSON file
-    // Fetch posts
-      async function fetchProduct() {
-        const response = await fetch('./review.json');
-        allReviws = await response.json();
-        displayData(allReviws);
-       
-    }
-    // Display users inside the div
-function displayData(reviews) {
-      
-  container.innerHTML = ""; // Clear previous data
+  async function fetchProduct() {
+      const response = await fetch('./review.json');
+      allReviews = await response.json();
+      displayData(allReviews);
+  }
 
-  reviews.forEach(review => {
-      let reviewDiv = document.createElement("div");
-      reviewDiv.classList.add("card"); // Add class for styling
-      reviewDiv.innerHTML = 
-      ` <div class="card-image">
-          <img src="${review.imgpath}" alt="Card Image" width="100%" height="100%">
-        </div>
-        
-        <div class="card-content">
-                      <h2 id="product-name"  class="card-title">${review.clientName
-            }
-                      </h2>
-
-                      ${'⭐'.repeat(review.srarrating)
-            }
-            
-                    
-                      <p class="card-text">${review.reviewText
-            }</p>
-
-             
-      `;
-      container.appendChild(reviewDiv);
-        });
-    }
-
-
+  // Display users inside the div
+  function displayData(reviews) {
+      container.innerHTML = ""; // Clear previous data
+      reviews.forEach(review => {
+          let reviewDiv = document.createElement("div");
+          reviewDiv.classList.add("card"); // Add class for styling
+          reviewDiv.innerHTML = `
+              <div class="card-image">
+                  <img src="${review.imgpath}" alt="Card Image" class="card-img">
+              </div>
+              <div class="card-content">
+                  <h2 id="product-name" class="card-title">${review.clientName}</h2>
+                  ${'⭐'.repeat(review.srarrating)}
+                  <p class="card-text">${review.reviewText}</p>
+              </div>
+          `;
+          container.appendChild(reviewDiv);
+      });
+  }
 
   fetchProduct();
 });
-
